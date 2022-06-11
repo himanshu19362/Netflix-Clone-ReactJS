885696efae3bd454af9207373d4737b2caf853e0
Himanshu <himanshu19362@iiitd.ac.in>
2022-06-11 13:50:31 +0530

    added 2 main sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import SectionList from './components/SectionList/SectionList';
 
 function App() {
   return (
     <>
       <Header />
+      <SectionList />
     </>
   );
 }
---
+++ src/components/Section/Section.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Section.css'
+// import vid from './../../videos/video1.mp4'
+
+function Section(props) {
+  return (
+    <div className='section'>
+        {props.messageLeft && <div className='message'>
+            <h1>{props.heading}</h1>
+            <p>{props.message}</p>
+        </div>
+        }
+        <div className='video'>
+            <video width="320" height="240" autoPlay loop muted><source src={props.videoUrl} type="video/mp4" /></video>
+        </div>
+
+        {!props.messageLeft && <div className='message' style={{"justify-self": "flex-start"}}>
+            <h1>{props.heading}</h1>
+            <p>{props.message}</p>
+        </div>
+        }
+    </div>
+  )
+}
+
+export default Section
